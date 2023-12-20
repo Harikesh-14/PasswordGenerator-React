0658eea17e097e8095f2a0fa3c973d13f3aba870
Harikesh-14 <68147228+Harikesh-14@users.noreply.github.com>
2023-12-20 15:14:30 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,90 @@
+import { useCallback, useEffect, useState } from 'react';
+import './App.css';
+
+function App() {
+    let [length, setLength] = useState(8);
+    let [numberAllowed, setNumberAllowed] = useState(false);
+    let [characterAllowed, setCharacterAllowed] = useState(false);
+    let [password, setPassword] = useState('');
+
+    let generatePassword = useCallback(() => {
+        let pass = '';
+        let str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+
+        if (numberAllowed) {
+            str += '0123456789';
+        }
+
+        if (characterAllowed) {
+            str += `[]{}-_=+()!@#$%^&*~;:""''<>/?,.`;
+        }
+
+        for (let i = 1; i <= length; i++) {
+            let char = Math.floor(Math.random() * str.length);
+            pass += str.charAt(char);
+        }
+        setPassword(pass);
+    }, [length, numberAllowed, characterAllowed, setPassword]);
+
+    useEffect(() => {
+        generatePassword()        
+    }, [length, setNumberAllowed, setCharacterAllowed, generatePassword])
+
+    return (
+        <>
+            <div className="mainContainer">
+                <h1>Password Generator</h1>
+
+                <div className="passwordContainer">
+                    <input
+                        type="text"
+                        value={password}
+                        placeholder="Password"
+                        className="passwordInput"
+                        readOnly
+                    />
+
+                    <button className="copyBtn">Copy</button>
+                </div>
+
+                <div className="passwordController">
+                    <div className="lengthController">
+                        <input
+                            type="range"
+                            className="passwordRange"
+                            min={8}
+                            max={60}
+                            value={length}
+                            onChange={(e) => {
+                                setLength(Number(e.target.value));
+                            }}
+                        />
+                        <label>Length: {length}</label>
+                    </div>
+
+                    <div className="numberAllowedController">
+                        <input
+                            type="checkbox"
+                            className="numberAllowed"
+                            checked={numberAllowed}
+                            onChange={() => setNumberAllowed((prev) => !prev)}
+                        />
+                        <label>Number Allowed</label>
+                    </div>
+
+                    <div className="characterAllowedController">
+                        <input
+                            type="checkbox"
+                            className="characterAllowed"
+                            checked={characterAllowed}
+                            onChange={() => setCharacterAllowed((prev) => !prev)}
+                        />
+                        <label>Character Allowed</label>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default App;
